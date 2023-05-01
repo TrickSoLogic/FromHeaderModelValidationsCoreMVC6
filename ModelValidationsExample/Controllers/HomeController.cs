8df2551357810087131f9046e123ced279b54b83
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-05-01 22:26:50 +0530

    Add project files.

---
+++ ModelValidationsExample/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FromHeaderModelValidationsCoreMVC6.Models;
+using FromHeaderModelValidationsCoreMVC6.CustomModelBinders;
+
+namespace FromHeaderModelValidationsCoreMVC6.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("register")]
+    public IActionResult Index(Person person, [FromHeader(Name = "User-Agent")] string UserAgent)
+    {
+      if (!ModelState.IsValid)
+      {
+        //get error messages from model state
+        string errors = string.Join("\n", ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage));
+  
+        return BadRequest(errors);
+      }
+
+      return Content($"{person}, {UserAgent}");
+    }
+  }
+}
